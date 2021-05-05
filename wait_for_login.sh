80aeae45bee8e9cef1df18c109de215277e654b9
Sebastien Besson <seb.besson@gmail.com>
2021-05-05 16:33:55 +0100

    Use new login --retry API

--- wait_for_login.sh
+++ wait_for_login.sh
@@ -7,14 +7,5 @@ OMERO_PASS=omero
 OMERO=/opt/omero/server/OMERO.server/bin/omero
 
 # Wait up to 2 mins
-i=0
-while ! docker-compose exec -T omeroserver $OMERO login -C -s localhost -u "$OMERO_USER" -q -w "$OMERO_PASS"; do
-    i=$(($i+1))
-    if [ $i -ge 24 ]; then
-        echo "$(date) - OMERO.server still not reachable, giving up"
-        exit 1
-    fi
-    echo "$(date) - waiting for OMERO.server..."
-    sleep 5
-done
+docker-compose exec -T omeroserver $OMERO login -C -s localhost -u "$OMERO_USER" -q -w "$OMERO_PASS" --retry 120
 echo "OMERO.server connection established"
