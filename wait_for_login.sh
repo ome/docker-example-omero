e7f0819b95076531e99140bed96a80b912d500e8
Ka Long <jackkykokoko@gmail.com>
2023-07-28 16:48:50 +0100

    Update wait_for_login.sh
    docker compose v2

--- wait_for_login.sh
+++ wait_for_login.sh
@@ -7,5 +7,5 @@ OMERO_PASS=omero
 OMERO=/opt/omero/server/OMERO.server/bin/omero
 
 # Wait up to 2 mins
-docker-compose exec -T omeroserver $OMERO login -C -s localhost -u "$OMERO_USER" -q -w "$OMERO_PASS" --retry 120
+docker compose exec -T omeroserver $OMERO login -C -s localhost -u "$OMERO_USER" -q -w "$OMERO_PASS" --retry 120
 echo "OMERO.server connection established"
